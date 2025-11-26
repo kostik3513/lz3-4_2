9c8c53aee8d73f229aaf6794824722638e2797ec
kostik3513 <kostik3513@gmail.com>
2025-11-26 16:13:01 +0200

    Add project files.

---
+++ lz3-4_2/lz3-4_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a = 0.0;
+    double b = 2.0;
+    int k;
+
+    cout << "Введіть k (натуральне число): ";
+    cin >> k;
+
+    double h = (b - a) / k;
+
+    cout << "\nТаблиця значень функції y = e^(3x^2 + 1)\n";
+    cout << "----------------------------------------\n";
+    cout << "   x\t\t   y\n";
+    cout << "----------------------------------------\n";
+
+    for (int i = 0; i <= k; i++) {
+        double x = a + i * h;
+        double y = exp(3 * x * x + 1);
+        cout << x << "\t\t" << y << endl;
+    }
+
+    return 0;
+}
